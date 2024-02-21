f24c59f1b64085fceeabc7d74eea96179b98d12f
LukasBeke <159446068+LukasBeke@users.noreply.github.com>
2024-02-21 18:50:19 +0200

    Delete python directory

--- python/pazimys.py
+++
@@ -1,8 +0,0 @@
-# Reikia patikrinti ar ivestas blogas(neegzistuojantis) pazimys.
-# -beg - 0 ir nuo 11 iki BEG
-
-pazimys = int(input('Pazimys'))
-if pazimys <=0 or pazimys >=11:
-    print(f'{pazimys} negalimas, kartokite ivedima')
-    pazimys = int(input('pazimys = '))
-print('pazimys = ' +str(pazimys))
